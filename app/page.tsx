987823fe17911ac3a0775a0fe290b2d9eee4a3e1
Tdev <tyler26daniel@gmail.com>
2025-09-27 23:28:23 -0400

    1

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,18 @@ export default function Home() {
             <div className="flex gap-5 items-center font-semibold">
               <Link href={"/"}>Next.js Supabase Starter</Link>
               <div className="flex items-center gap-2">
+                
                 <DeployButton />
-              </div>
+              </div> 
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+        <div>
+              <img src="static/images/demon-f.png" alt="Demon" />
+              </div>
+        
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5"> 
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
